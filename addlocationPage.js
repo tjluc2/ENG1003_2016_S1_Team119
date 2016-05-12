fc60662637f5b2e46ab9893a78cf6fb9cd9e7371
lawal8 <lawal8@student.monash.edu.au>
2016-05-12 21:20:59 +1000

    Added api key and changed function name. Rearranged code in addlocationpage to reduce errors
    API key and function change in the html
    In the js I changed the code around so stop errors with null values etc.

--- addlocationPage.js
+++ addlocationPage.js
@@ -10,69 +10,65 @@ function initialize() {
   };
    map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
-function addLocation() {
-        			var geocoder = new google.maps.Geocoder();    // instantiate a geocoder object
+function geoLocate() {
+            var geocoder = new google.maps.Geocoder();    // instantiate a geocoder object
 			
 			// Get the user's inputted address
 			var address = document.getElementById( "address" ).value;
 		  
 			// Make asynchronous call to Google geocoding API
-			geocoder.geocode( { 'address': address }, function(results, status) {
-				var addr_type = results[0].types[0];	// type of address inputted that was geocoded
+    geocoder.geocode( { 'address': address }, function(results, status) {
+				// type of address inputted that was geocoded
+                // formattedAddress = results[0].formatted_address
+        if ( status == google.maps.GeocoderStatus.OK){       
+                map.setCenter(results[0].geometry.location);
                 formattedAddress = results[0].formatted_address
-				if ( status == google.maps.GeocoderStatus.OK ){  
-                    ShowLocation( results[0].geometry.location, address, addr_type );
+                var addr_type = results[0].types[0];
+                var nickname = document.getElementById("nickname").value;
+        
+                // Place a Google Marker at the same location as the map center 
+                // When you hover over the marker, it will display the title
+                var marker = new google.maps.Marker( { 
+                    position: results[0].geometry.location,     
+                    map: map,  
+                    title: address
+                });
+		
+		      // Create an InfoWindow for the marker
+                if (nickname.length > 0) {
+                    var contentString = "<b>" + nickname + "</b>";	// HTML text to display in the InfoWindo
+                } else {
+                    var contentString = "<b>" + formattedAddress + "</b>";	// HTML text to display in the InfoWindow
                 }
-				else     {
-					alert("Geocode was not successful for the following reason: " + status);    
+                var infowindow = new google.maps.InfoWindow( { content: contentString } );
+                var lat =results[0].geometry.location.lat();
+                var lng = results[0].geometry.location.lng()
+                    var locationCacheInstance = new LocationWeatherCache();
+                locationCacheInstance.addLocation(lat, lng, nickname);
+            
+		
+                // Set event to display the InfoWindow anchored to the marker when the marker is clicked.
+                google.maps.event.addListener( marker, 'click', function() { infowindow.open( map, marker ); });
+                // Set the zoom level according to the address level of detail the user specified
+                var zoom = 12;
+                switch ( addr_type )
+                {
+                    case "administrative_area_level_1"	: zoom = 6; break;		// user specified a state
+		            case "locality"						: zoom = 10; break;		// user specified a city/town
+                    case "street_address"				: zoom = 15; break;		// user specified a street address
                 }
+                map.setZoom( zoom );
+                }
+        else if (document.getElementById( "address" ).value != "" && status != "OVER_QUERY_LIMIT")    {
+				alert("Geocode was not successful for the following reason: " + status);    
+                document.getElementById( "address" ).value = "";
+                
+                    
+                    }
+
 			});
+	
 		} ;
-
-	// Show the location (address) on the map.
-	function ShowLocation( latlng, address, addr_type )
-	{
-		// Center the map at the specified location
-		map.setCenter( latlng );
-		
-		// Set the zoom level according to the address level of detail the user specified
-		var zoom = 12;
-		switch ( addr_type )
-		{
-		case "administrative_area_level_1"	: zoom = 6; break;		// user specified a state
-		case "locality"						: zoom = 10; break;		// user specified a city/town
-		case "street_address"				: zoom = 15; break;		// user specified a street address
-		}
-		map.setZoom( zoom );
-        
-		//Get the user's inputted nickname
-        var nickname = document.getElementById("nickname").value;
-        
-		// Place a Google Marker at the same location as the map center 
-		// When you hover over the marker, it will display the title
-		var marker = new google.maps.Marker( { 
-			position: latlng,     
-			map: map,  
-            title: address
-		});
-		
-		// Create an InfoWindow for the marker
-        if (nickname.length > 0) {
-		      var contentString = "<b>" + nickname + "</b>";	// HTML text to display in the InfoWindo
-        } else {
-              var contentString = "<b>" + formattedAddress + "</b>";	// HTML text to display in the InfoWindow
-        }
-        var infowindow = new google.maps.InfoWindow( { content: contentString } );
-        var lat = latlng.lat();
-        var lng = latlng.lng()
-        var locationCacheInstance = new LocationWeatherCache();
-        locationCacheInstance.addLocation(lat, lng, nickname);
-        
-		
-		// Set event to display the InfoWindow anchored to the marker when the marker is clicked.
-		google.maps.event.addListener( marker, 'click', function() { infowindow.open( map, marker ); });
-	}
-
-
